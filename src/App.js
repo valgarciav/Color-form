ac3ab7a17d43d75e45b90d33e51bdcdc1733b65c
valgarciav <vkgarcia@gmail.com>
2022-06-09 23:09:21 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import React, { useState } from "react";
+import ColorBlock from "../src/ColorBlock";
+import ColorForm from "./ColorForm";
+
+function App() {
+  let [colors, setColors] = useState([
+    "violet",
+    "blue",
+    "lightblue",
+    "green",
+    "greenyellow",
+    "yellow",
+    "orange",
+    "red",
+  ]);
+
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock color={color} />;
+  });
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor]);
+  };
+
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
+  );
+}
+
+export default App;
